e1f0b5c37b27c487e454c420f903430346a497da
AideenM12 <a.malone1289@gmail.com>
2021-05-26 14:07:34 +0000

    save

---
+++ app.py
@@ -0,0 +1,17 @@
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == "POST":
+        # check if username is already in use
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This username already exists")
+            return redirect(url_for("registration")) 
+
+        sign-up{
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")).lower(),
+            "email": request.form.get("email")
+        }
+    return render_template("sign-up.html")
